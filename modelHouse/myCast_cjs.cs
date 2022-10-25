d7ace44628307526c989cd619655810beaa12fd4
Peterhabit <jiseokchoij@gmail.com>
2022-10-26 01:15:57 +0900

    feat: 태그를 이용하여 Raycast에 충돌 된 객체 이동

--- modelHouse/myCast_cjs.cs
+++ modelHouse/myCast_cjs.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class myCast_cjs : MonoBehaviour
 {
+    public GameObject head;
+    public GameObject pos1;
     void Update()
     {
         RaycastHit hit;
@@ -11,8 +13,21 @@ public class myCast_cjs : MonoBehaviour
         Debug.DrawRay(transform.position, forward, Color.green);
         if(Physics.Raycast(transform.position, forward, out hit))
         {
+            if (hit.collider.tag == "reTag")
+            {
+                StartCoroutine(moveCo());
+            }
             Debug.Log(hit.transform.name);
         }
         
+    }
+    
+    IEnumerator moveCo()
+    {
+        while(head.transform.position != pos1.transform.position)
+        {
+            head.transform.position = Vector3.MoveTowards(head.transform.position, pos1.transform.position, Time.deltaTime * 0.08f);
+            yield return null;
+        }
     }
 }
